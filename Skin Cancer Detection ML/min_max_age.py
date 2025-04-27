f91c3e4b381892c3b11820889d57286dd7ca7174
ShalinPatel670 <patel.5469@buckeyemail.osu.edu>
2025-04-27 14:55:14 -0400

    Update min_max_age.py

--- Skin Cancer Detection ML/min_max_age.py
+++ Skin Cancer Detection ML/min_max_age.py
@@ -2,21 +2,18 @@ import pandas as pd
 import numpy as np
 from tqdm import tqdm
 
-# Load the CSV data into a pandas DataFrame
+# Load CSV data into dataframe
 data = pd.read_csv(r"C:\Users\16145\OneDrive\Desktop\skin_ml\540_metadata_age_minmax.csv")
 
-# Extract the age column and reshape for scaling
 age_data = data.iloc[:, 1].values.reshape(-1, 1)  # Assuming age is in the second column
 
-# Impute missing values with the mean of non-missing values
 mean_age = np.mean(age_data[~np.isnan(age_data)])
 age_data[np.isnan(age_data)] = mean_age
 
-# Calculate the minimum and maximum values of the age column
+# Calculate min and max of age
 min_age = np.min(age_data)
 max_age = np.max(age_data)
 
-# Initialize tqdm for the normalization process
 with tqdm(total=len(age_data), desc="Normalizing Age") as pbar:
     # Perform Min-Max normalization manually
     age_normalized = (age_data - min_age) / (max_age - min_age)
@@ -27,7 +24,7 @@ with tqdm(total=len(age_data), desc="Normalizing Age") as pbar:
 # Replace the original age column with the normalized values
 data.iloc[:, 1] = age_normalized  # Assuming age is in the second column
 
-# Save the preprocessed data to a CSV file
+# Save to csv
 output_file_path = r"C:\Users\16145\OneDrive\Desktop\skin_ml\preprocessed_min_max_age_data.csv"
 data.to_csv(output_file_path, index=False)  # Use 'data' instead of 'data_encoded'
 
