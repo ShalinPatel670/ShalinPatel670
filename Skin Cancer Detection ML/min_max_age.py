005b36ac2cc43b3c9e4c4ebf3e7a5de53fe687d4
ShalinPatel670 <patel.5469@buckeyemail.osu.edu>
2025-04-27 15:30:56 -0400

    Update min_max_age.py

--- Skin Cancer Detection ML/min_max_age.py
+++ Skin Cancer Detection ML/min_max_age.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 from tqdm import tqdm
 
-# Load CSV data into dataframe
 data = pd.read_csv(r"C:\Users\16145\OneDrive\Desktop\skin_ml\540_metadata_age_minmax.csv")
 
 age_data = data.iloc[:, 1].values.reshape(-1, 1)  # Assuming age is in the second column
@@ -18,13 +17,10 @@ with tqdm(total=len(age_data), desc="Normalizing Age") as pbar:
     # Perform Min-Max normalization manually
     age_normalized = (age_data - min_age) / (max_age - min_age)
     
-    # Update tqdm progress bar
     pbar.update(len(age_data))
 
-# Replace the original age column with the normalized values
-data.iloc[:, 1] = age_normalized  # Assuming age is in the second column
+data.iloc[:, 1] = age_normalized  
 
-# Save to csv
 output_file_path = r"C:\Users\16145\OneDrive\Desktop\skin_ml\preprocessed_min_max_age_data.csv"
 data.to_csv(output_file_path, index=False)  # Use 'data' instead of 'data_encoded'
 
